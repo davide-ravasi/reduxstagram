888ba2fe4f8eb3f206bf8b6a044c6dd009e0d690
davide-ravasi <info@davideravasi.com>
2017-03-13 17:41:43 +0100

    added single post

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,38 +1,36 @@
 import React from 'react';
 
 const Comments = React.createClass({
-  renderComment(comment, i) {
-    return (
-      <div className="comment" key={i}>
-        <p>
-          <strong>{comment.user}</strong>
-          {comment.text}
-          <button className="remove-comment" onClick={this.props.removeComment.bind(null,this.props.params.postId, i)}>&times;</button>
-        </p>
-      </div>
-    );
-  },
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.addComment(this.props.params.postId, this.refs.author.value, this.refs.comment.value);
-    this.refs.commentForm.reset();
-  },
-  render() {
-
-    const comments = this.props.comments[this.props.params.postId] || [];
-    return (
-      <div className="comments">
-
-        {comments.map(this.renderComment)}
-
-        <form onSubmit={this.handleSubmit} ref="commentForm" className="comment-form">
-          <input type="text" ref="author" placeholder="author"/>
-          <input type="text" ref="comment" placeholder="comment"/>
-          <input type="submit" hidden/>
-        </form>
-      </div>
-    );
-  }
+    renderComment(comment, i) {
+        return (
+            <div className="comment" key={i}>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment">&times;</button>
+            </div>
+        )
+    },
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('submitting the form');
+        console.log(this.refs);
+        const postId = this.props.params.postId;
+        const author = this.refs.author.value;
+        const comment = this.refs.comment.value;
+        console.log(postId, author, comment);
+    },
+    render() {
+        return (
+            <div className="comment">
+                {this.props.postComments.map(this.renderComment)}
+                <form ref="commentForm" className="comment-form" onSubmit={this.handleSubmit}>
+                    <input type="text" ref="author" placeholder="author"/>
+                    <input type="text" ref="comment" placeholder="comment"/>
+                    <input type="submit" hidden />
+                </form>
+            </div>
+        )
+    }
 });
 
 export default Comments;
--- client/components/Single.js
+++ client/components/Single.js
@@ -2,17 +2,19 @@ import React from 'react';
 import Photo from './Photo';
 import Comments from './Comments';
 
+
 const Single = React.createClass({
-
   render() {
-    const i = this.props.posts.findIndex((post)=> post.code === this.props.params.postId);
-
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[this.props.params.postId];
+    console.log(post);
     return (
       <div className="single-photo">
-        <Photo key={i} i={i} post={this.props.posts[i]} {...this.props} />
-        <Comments {...this.props} postId={i} />
+        <Photo {...this.props} key={i} i={i} post={post}  />
+        <Comments postComments={postComments} {...this.props} />
       </div>
-    );
+    )
   }
 });
 
